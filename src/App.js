952a5bc09e2e2f684d23d4476e58e28016722b02
Vlad <vladklivak@gmail.com>
2019-04-06 21:09:11 +0300

    Added main components to app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                // add components here
+                <Titles/>
+                <Form/>
+                <Weather/>
             </div>
         )
     }
